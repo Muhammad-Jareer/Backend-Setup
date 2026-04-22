d35d8b0dca628d5519cf0e26ed6d933ba76395a4
Muhammad jareer <jareerameer@gmail.com>
2026-04-22 11:28:08 +0430

    Remove .env from tracking

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise
+//         .resolve(requestHandler(req, res, next))
+//         .catch((error) => next(error))
+//     }
+// }
+
+
+const asyncHandler = async (requestHandler) => {
+    return async(req, res, next ) => {
+        try {
+            await requestHandler(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+// const asyncHandler = (requestHandler) => (req, res, next) => {
+//     try {
+//         await requestHandler(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json ({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+
+export default asyncHandler;
