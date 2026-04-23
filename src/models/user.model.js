5cb5fab5be6992f1b24dabd77a19c07d639cde99
Muhammad jareer <jareerameer@gmail.com>
2026-04-23 09:06:55 +0430

    adding Aggregate Paginate Plugin

---
+++ src/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema (
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        avatar: {
+            type: String,
+            required: true
+        },
+        coverImage: {
+            type: String,
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "password is required"],
+        },
+        refereshToken: {
+            type: String
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+
+export const User = mongoose.model("User", userSchema)
